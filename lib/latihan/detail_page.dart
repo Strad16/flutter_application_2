2e1bebb845d3b7bd9ff13b24e652f6a9c5d30df2
Strad16 <putraaryanto1611@gmail.com>
2025-09-15 10:47:37 +0700

    update detail_page.dart

--- lib/latihan/detail_page.dart
+++ lib/latihan/detail_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -7,7 +9,9 @@ class DetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Column(
+      body: SafeArea(
+        child : SingleChildScrollView(
+        child: Column(
         children: [
           Image.asset("1.jpeg"),
           Container(
@@ -34,12 +38,45 @@ class DetailPage extends StatelessWidget {
           Container(
             padding: const EdgeInsets.symmetric(vertical: 50),
             child: Text(
-              "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", 
+              "aaaaaaaaaaaaaaaaaaaaaaaaaaa....", 
               textAlign: TextAlign.center,
+              overflow: TextOverflow.ellipsis,
+              maxLines: 3, 
             ),
           ),
+          //scroll horizontal
+          SizedBox(height: 150,
+            child: ScrollConfiguration(behavior: ScrollConfiguration.of
+            (context).copyWith(
+              dragDevices: {
+                PointerDeviceKind.touch,
+                PointerDeviceKind.mouse,
+                PointerDeviceKind.stylus,
+              }
+            ),
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                Padding(padding: const EdgeInsets.all(8.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.network('https://s-light.tiket.photos/t/01E25EBZS3W0FY9GTG6C42E1SE/rsfit1440960gsm/events/2025/01/06/8c3726e3-b66e-43e5-ba40-aeb1a2ac13f5-1736147153269-b486cf97eb9f0f8feb5b22faa9b20937.png'),
+                ),
+                ),
+                Padding(padding: const EdgeInsets.all(8.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.network('https://s-light.tiket.photos/t/01E25EBZS3W0FY9GTG6C42E1SE/rsfit1440960gsm/events/2025/01/06/8c3726e3-b66e-43e5-ba40-aeb1a2ac13f5-1736147153269-b486cf97eb9f0f8feb5b22faa9b20937.png'),
+                ),
+                )
+              ],
+            ),
+            ),
+          )
         ],
+        )
       )),
+
     );
   }
 }
